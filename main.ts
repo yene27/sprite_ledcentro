e4ff421d29f4a9cff6d27d55f05d5bebf4eb7a67
yene27 <yene27@yahoo.com>
2022-09-01 12:01:54 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+let sprite = game.createSprite(2, 2)
+game.setScore(0)
+game.setLife(2)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+        basic.pause(200)
+        if (sprite.get(LedSpriteProperty.X) == 2) {
+            basic.showIcon(IconNames.Happy)
+            game.setScore(game.score() + 1)
+        } else {
+            basic.showIcon(IconNames.Sad)
+            game.removeLife(1)
+        }
+    }
+    sprite.move(1)
+    sprite.ifOnEdgeBounce()
+})
